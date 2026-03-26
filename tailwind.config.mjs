89a6c0cbf4fcf96380b791d91c989f6ea2a7c7d3
Pavel Moravec <pavel.moravec@gmail.com>
2026-03-26 19:48:04 +0100

    code cleanup

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,9 @@ export default {
 				black: '#1a1a1a',
 				muted: '#888',
 				gray: '#f5f5f3',
+				prose: '#333333',
+				faint: '#bbbbbb',
+				line: '#e5e5e5',
 			},
 			fontFamily: {
 				sans: ['DM Sans', 'sans-serif'],
