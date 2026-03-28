5fe3cd2d7da6bea505c2d44694f484042700ba79
Pavel Moravec <pavel.moravec@gmail.com>
2026-03-28 19:10:06 +0100

    improve transitions and add linting

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Zuzi Knaze';
-export const SITE_DESCRIPTION = 'Interior creator based in The Hague, working at the intersection of architecture, spatial design, and everyday life.';
+export const SITE_TITLE = "Zuzi Knaze";
+export const SITE_DESCRIPTION =
+  "Interior creator based in The Hague, working at the intersection of architecture, spatial design, and everyday life.";
